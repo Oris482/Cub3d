510363546a18adb79ce9cfa71663c486e9e98d50
Jaemin Jeong <jaemjeon@c8r8s2.42seoul.kr>
2022-10-20 19:30:12 +0900

    clean_code : remove useless comment & reset texture code [todo] merge main and start texture#8

--- ray_casting_forked_from_check_argv/ray_casting.c
+++ ray_casting_forked_from_check_argv/ray_casting.c
@@ -1,6 +1,5 @@
 #include "../cub3d.h"
 #include "mlx.h"
-#include <unistd.h>
 
 void	init_ray(t_game *game)
 {
@@ -34,8 +33,8 @@ void	set_ray_step(t_vector2 *vec_pos, t_ray *ray, t_vector2 *step)
 			step->y = -1;
 	}
 	{
-		ray->hit_idx_x = (int)(vec_pos->x);
-		ray->hit_idx_y = (int)(vec_pos->y);
+		ray->hit_idx_x = floor(vec_pos->x);
+		ray->hit_idx_y = floor(vec_pos->y);
 	}
 }
 
@@ -54,8 +53,6 @@ void	set_dist(t_vector2 *pos, double cast_angle, \
 		side_dist->y = delta_dist->y * (ceil(pos->y) - pos->y);
 	else
 		side_dist->y = delta_dist->y * (pos->y - floor(pos->y));
-	// side_dist->x = (fabs(floor(pos->x) - pos->x) + 1) * delta_dist->x;
-	// side_dist->y = (fabs(floor(pos->y) - pos->y) + 1) * delta_dist->y;
 }
 
 void	get_hit_wall_side(t_ray *ray, int last_step)
@@ -74,7 +71,6 @@ void	get_hit_wall_side(t_ray *ray, int last_step)
 		else
 			ray->hit_wall_side = 2;
 	}
-	// printf("%d", ray->hit_wall_side);
 }
 
 int	ray_travel(t_ray *ray, t_vector2 *step, \
@@ -82,18 +78,6 @@ int	ray_travel(t_ray *ray, t_vector2 *step, \
 {
 	int		last_step;
 
-	// if (side_dist->x < side_dist->y + delta_dist->y)
-	// {
-	// 	side_dist->x += delta_dist->x;
-	// 	ray->hit_idx_x += step->x;
-	// 	last_step = 0;
-	// }
-	// else
-	// {
-	// 	side_dist->y += delta_dist->y;
-	// 	ray->hit_idx_y += step->y;
-	// 	last_step = 1;
-	// }
 	if (side_dist->y < side_dist->x)
 	{
 		side_dist->y += delta_dist->y;
@@ -116,11 +100,8 @@ void	calcul_hitpoint_dist(t_player *player_info, t_ray *ray)
 	double			delta_angle;
 
 	delta_angle = fabs(player_info->camera_angle - ray->cast_angle);
-	// while (delta_angle >= 360)
-	// 	delta_angle -= 360;
 	ray->ray_length = sqrt(pow(delta_x, 2) + pow(delta_y, 2));
 	ray->wall_distance = fabs(cos(degree_to_radian(delta_angle))) * ray->ray_length;
-	// ray->wall_distance = ray->ray_length;
 }
 
 void	calcul_texture_x_point(t_vector2 *cast_point, t_ray *ray)
@@ -161,17 +142,6 @@ void	set_ray_hit_point(t_vector2 *vec_pos, t_ray *ray)
 		ray->hit_point.x = \
 			vec_pos->x + (ray->hit_point.y - vec_pos->y) * tan(degree_to_radian(ray->cast_angle));
 	}
-	// printf("wall side : %d\n", ray->hit_wall_side);
-	// printf("hit_point x : %lf y : %lf\n", ray->hit_point.x, ray->hit_point.y);
-	// if (ray->hit_point.x < 0 || ray->hit_point.y < 0)
-	// {
-	// 	printf("vecpos x:%lf  y:%lf\n", vec_pos->x, vec_pos->y);
-	// 	printf("%d %d\n", ray->hit_idx_x, ray->hit_idx_y);
-	// 	printf("cast_angle : %lf\n", ray->cast_angle);
-	// 	printf("sin(cast_angle) = %lf\n", sin(ray->cast_angle));
-	// 	// printf("%lf\n", vec_pos->y + (ray->hit_point.x - vec_pos->x) * (-0.5));
-	// 	sleep(1);
-	// }
 }
 
 void	new_calcul_hitpoint_dist(t_ray *ray, int last_step, \
@@ -185,34 +155,6 @@ void	new_calcul_hitpoint_dist(t_ray *ray, int last_step, \
 
 void	reflect_hit_point(t_vector2 *cast_pos, t_ray *ray, double abs_delta_len)
 {
-	// if (ray->hit_wall_side == 0)
-	// {
-	// 	if (90 < ray->cast_angle && ray->cast_angle <= 180)
-	// 		ray->hit_point.y = cast_pos->y + abs_delta_len + 1;
-	// 	else
-	// 		ray->hit_point.y = cast_pos->y + abs_delta_len;
-	// }
-	// else if (ray->hit_wall_side == 1)
-	// {
-	// 	if (ray->cast_angle <= 90)
-	// 		ray->hit_point.y = cast_pos->y + abs_delta_len + 1;
-	// 	else
-	// 		ray->hit_point.y = cast_pos->y + abs_delta_len;
-	// }
-	// else if (ray->hit_wall_side == 2)
-	// {
-	// 	if (ray->cast_angle < 270)
-	// 		ray->hit_point.x = cast_pos->x + abs_delta_len;
-	// 	else
-	// 		ray->hit_point.x = cast_pos->x + abs_delta_len + 1;
-	// }
-	// else
-	// {
-	// 	if (ray->cast_angle < 90)
-	// 		ray->hit_point.x = cast_pos->x + abs_delta_len + 1;
-	// 	else
-	// 		ray->hit_point.x = cast_pos->x + abs_delta_len;
-	// }
 	if (ray->hit_wall_side == 2 || ray->hit_wall_side == 3)
 	{
 		if (cos(degree_to_radian(ray->cast_angle)) > 0)
@@ -262,8 +204,6 @@ void	new_set_wall_distance(t_player *player_info, t_ray *ray)
 
 	delta_angle = fabs(player_info->camera_angle - ray->cast_angle);
 	ray->wall_distance = cos(degree_to_radian(delta_angle)) * ray->ray_length;
-	// printf("%lf\n", ray->ray_length);
-	// ray->wall_distance = ray->ray_length;
 }
 
 void	cast_sigle_ray(t_game *game, t_ray *ray)
@@ -272,14 +212,9 @@ void	cast_sigle_ray(t_game *game, t_ray *ray)
 	t_vector2	side_dist;
 	t_vector2	delta_dist;
 	int			last_step;
-	// static int			debug_i = 0;
 
 	set_ray_step(&game->player.vec_pos, ray, &step);
 	set_dist(&game->player.vec_pos, ray->cast_angle, &side_dist, &delta_dist);
-	// printf("idx : %d side_dist (%lf %lf), delta_dist (%lf %lf)\n", debug_i, side_dist.x, side_dist.y, delta_dist.x, delta_dist.y);
-	// debug_i++;
-	// if (debug_i == game->info.screen_x)
-	// 	exit(0);
 	while (TRUE)
 	{
 		last_step = ray_travel(ray, &step, &side_dist, &delta_dist);
@@ -290,23 +225,8 @@ void	cast_sigle_ray(t_game *game, t_ray *ray)
 	new_calcul_hitpoint_dist(ray, last_step, &side_dist, &delta_dist);
 	new_set_ray_hit_point(&game->player.vec_pos, ray);
 	new_set_wall_distance(&game->player, ray);
-	// set_ray_hit_point(&game->player.vec_pos, ray);
-	// printf("hit_x : %lf hit_y : %lf\n", ray->hit_point.x, ray->hit_point.y);
-	// calcul_hitpoint_dist(&game->player, ray, &side_dist, &delta_dist);
 	calcul_texture_x_point(&game->player.vec_pos, ray);
 }
-
-// void	set_cast_angle(t_ray, int idx_x)
-// {
-// 	double			cast_angle;
-
-// 	cast_angle = game->player.camera_angle - (game->info.fov_h / 2) + delta_angle * idx_x;
-// 	if (cast_angle < 0)
-// 		cast_angle += 360;
-// 	else if (cast_angle >= 360)
-// 		cast_angle -= 360;
-// 	game->ray_data[idx_x].cast_angle = cast_angle;
-// }
 
 void	debug_print_ray_info(t_game *game)
 {
@@ -342,17 +262,13 @@ void	ray_cast(t_game *game)
 			cast_angle -= 360.0;
 		game->ray_data[idx_x].cast_angle = cast_angle;
 		cast_sigle_ray(game, &game->ray_data[idx_x]);
-		// printf("%d :  x:%d y:%d\n", idx_x, game->ray_data[idx_x].hit_idx_x, game->ray_data[idx_x].hit_idx_y);
 		idx_x++;
 	}
-	// debug_print_ray_info(game);
 }
 
 void	calcul_drawpixel(t_game *game, t_ray *ray, t_vector2 *wall_pixel)
 {
 	const double	ratio_wall = 1 / ray->wall_distance;
-	// wall_pixel->x = ray->wall_distance * 50; // 그냥 대입해봄
-	// wall_pixel->y = game->info.screen_y - ray->wall_distance * 50;
 
 	wall_pixel->x = game->info.screen_y / 2 - ratio_wall * game->info.screen_y;
 	wall_pixel->y = game->info.screen_y / 2 + ratio_wall * game->info.screen_y;
@@ -360,19 +276,6 @@ void	calcul_drawpixel(t_game *game, t_ray *ray, t_vector2 *wall_pixel)
 		wall_pixel->x = 0;
 	if (wall_pixel->y > game->info.screen_y)
 		wall_pixel->y = game->info.screen_y;
-}
-
-int	get_wall_texture_pixel(t_game *game, t_vector2 *wall_pixel, int idx_x, int idx_y)
-{
-	t_vector2		texture_idx;
-	unsigned int	pixel_color;
-
-	texture_idx.x = game->ray_data[idx_x].hit_texture_point * \
-			game->texture[game->ray_data[idx_x].hit_wall_side].texture_width;
-	texture_idx.y = (idx_y - wall_pixel->x) * (wall_pixel->y - wall_pixel->x) * \
-			game->texture[game->ray_data[idx_x].hit_wall_side].texture_height;
-	pixel_color = game->texture[game->ray_data[idx_x].hit_wall_side].img;
-	// return (create_trgb(0, get_r()))
 }
 
 void	draw_line(t_game *game, int idx_x, t_vector2 *wall_pixel)
@@ -394,7 +297,6 @@ void	draw_line(t_game *game, int idx_x, t_vector2 *wall_pixel)
 	{
 		dst = bg_data->addr + (idx_y * bg_data->line_length +
 							   idx_x * (bg_data->bits_per_pixel / 8));
-		// *(unsigned int *)dst = get_wall_texture_pixel(game, wall_pixel, idx_x, idx_y);
 		if (game->ray_data[idx_x].hit_wall_side == 0)
 			*(unsigned int *)dst = create_trgb(0, 255, 0, 0);
 		else if (game->ray_data[idx_x].hit_wall_side == 1)
@@ -425,40 +327,18 @@ void	make_wall(t_game *game, t_vector2 *wall_pixel)
 		draw_line(game, idx_x, wall_pixel);
 		idx_x++;
 	}
-	// sleep(10);
 }
 
-void	draw_wall(t_game *game, t_vector2 *wall_pixel)
+void	draw(t_game *game, t_vector2 *wall_pixel)
 {
 	ray_cast(game);
 	make_wall(game, wall_pixel);
 }
 
-void	draw_back_ground(t_game *game, t_vector2 *wall_pixel)
-{
-	//	천장이랑 바닥 채우기
-}
-
-void	reset_img(t_game *game)
-{
-	size_t	size_of_img;
-
-	size_of_img = game->bg_data.line_length * game->info.screen_y + \
-				game->info.screen_x * (game->bg_data.bits_per_pixel / 8);
-	ft_memset(game->bg_data.addr, 0, sizeof(game->bg_data.img));
-}
-
 void	draw_screen(t_game *game)
 {
-	static int	frame = 0;
 	t_vector2	wall_pixel;
 
-	draw_wall(game, &wall_pixel);
-	draw_back_ground(game, &wall_pixel);
-	// 그리기
-	// mlx_clear_window(game->info.mlx_ptr, game->info.win_ptr);
+	draw(game, &wall_pixel);
 	mlx_put_image_to_window(game->info.mlx_ptr, game->info.win_ptr, game->bg_data.img, 0, 0);
-	// reset_img(game);
-	// printf("frame : %d\n", frame);
-	frame++;
 }
